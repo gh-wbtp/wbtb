8ea2522b5304661b49d15174d451efdbc16226c8
boyninja1555 <flappyfloorg@gmail.com>
2026-09-18 15:26:50 -0500

    Now rendering actual page content!! Small set of WBL tags though... NOT FOR LONG!!

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
+#define _POSIX_C_SOURCE 199309L
+#include <time.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include <signal.h>
 #include <stdatomic.h>
 #include <stdlib.h>
@@ -54,6 +61,54 @@ char *read_stdin(size_t *length)
     return buf;
 }
 
+typedef struct
+{
+    u8 x, y;
+    bool center;
+    bool heading;
+} RenderState;
+
+void render_node(WblNode *node, RenderState *state)
+{
+    for (u32 i = 0; i < node->children_length; i++)
+    {
+        WblNode *child = &node->children[i];
+        if (child->type == WBL_TEXT)
+        {
+            size_t text_length = strlen(child->text);
+            u8 text_x = state->center && text_length < WBTBT_WIDTH ? (WBTBT_WIDTH - text_length) / 2 : state->x;
+            terminal_text((WbtbtText){.text = child->text, .max_length = WBTBT_NOWRAP, .x = text_x, .y = state->y++});
+
+            if (state->heading)
+            {
+                char *text = malloc(text_length + 1);
+                for (u16 j = 0; j < text_length; j++)
+                    text[j] = '-';
+                text[text_length] = '\0';
+                terminal_text((WbtbtText){.text = text, .max_length = WBTBT_NOWRAP, .x = text_x, .y = state->y++});
+                free(text);
+            }
+        }
+        else if (child->type == WBL_ELEMENT)
+        {
+            bool is_heading = strcmp(child->tag, "h1") == 0 || strcmp(child->tag, "h2") == 0 || strcmp(child->tag, "h3") == 0;
+            bool is_center = strcmp(child->tag, "center") == 0;
+
+            if (is_heading)
+                state->heading = true;
+            else if (is_center)
+                state->center = true;
+
+            render_node(child, state);
+
+            if (is_heading)
+                state->heading = false;
+            else if (is_center)
+                state->center = false;
+        }
+    }
+}
+
 int main(void)
 {
     if (signal(SIGINT, sigint_callback) == SIG_ERR)
@@ -84,10 +139,21 @@ int main(void)
     char buf[4096];
     size_t written = wbl_node_stringify(parser.root, buf, 4095);
     buf[written] = '\0';
-    terminal_text((WbtbtText){.text = buf, .max_length = WBTBT_WIDTH - 10, .x = 10, .y = 10});
+
+    RenderState render_state = {.x = 0, .y = 0};
+    render_node(&parser.root, &render_state);
 
     while (atomic_load(&running))
+    {
         terminal_render(stdout);
+
+#ifdef _WIN32
+        Sleep(17);
+#else
+        struct timespec frame_time = {.tv_sec = 0, .tv_nsec = 16666667};
+        nanosleep(&frame_time, NULL);
+#endif
+    }
 
     terminal_cleanup(stdout);
     parser.free(&parser);
